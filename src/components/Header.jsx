9bdaba0675c3c4b8e699f1a7afaa556a5a26d640
daniele.gattuso <d.gattuso09@gmail.com>
2025-02-12 16:58:29 +0100

    made header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,35 @@
 export default function Header() {
     return (
         <header>
-            <figure>
-                <img src="../img/dc-logo.png" alt="Logo" />
-            </figure>
 
-            <nav>
-                <ul>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#" className="active">COMICS</a></li>
-                    <li><a href="#">MOVIES</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">GAMES</a></li>
-                    <li><a href="#" className="active">COLLETIBLES</a></li>
-                    <li><a href="#">VIDEOS</a></li>
-                    <li><a href="#">FANS</a></li>
-                    <li><a href="#">NEWS</a></li>
-                    <li><a href="#">SHOP</a></li>
-                </ul>
-            </nav>
+            <div className="header_container">
+
+                <div className="logo_container">
+                    <figure>
+                        <img src="../img/dc-logo.png" alt="Logo" />
+                    </figure>
+                </div>
+
+                <div className="nav_container">
+                    <nav>
+                        <ul>
+                            <li><a href="#">CHARACTERS</a></li>
+                            <li><a href="#" className="active">COMICS</a></li>
+                            <li><a href="#">MOVIES</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">GAMES</a></li>
+                            <li><a href="#" className="active">COLLETIBLES</a></li>
+                            <li><a href="#">VIDEOS</a></li>
+                            <li><a href="#">FANS</a></li>
+                            <li><a href="#">NEWS</a></li>
+                            <li><a href="#">SHOP</a></li>
+                        </ul>
+                    </nav>
+
+                </div>
+
+            </div>
+
         </header>
     );
 }
