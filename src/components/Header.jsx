07b3c3caa6b410ebf6da8e796567eaf4876f7c11
daniele.gattuso <d.gattuso09@gmail.com>
2025-02-12 16:42:05 +0100

    Start layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,25 @@
 // Header
 export default function Header() {
-    return (<header><h1>aaaaaa</h1></header>);
+    return (
+        <header>
+            <figure>
+                <img src="../img/dc-logo.png" alt="Logo" />
+            </figure>
+
+            <nav>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#" className="active">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#" className="active">COLLETIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
