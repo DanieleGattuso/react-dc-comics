026f32c4660900617723a42e554ec9f5d0c74dbc
daniele.gattuso <d.gattuso09@gmail.com>
2025-02-13 15:44:17 +0100

    made main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,38 @@ export default function Main() {
     return (
         <main>
             <div >
-                <div >
-                    {">"} Content goes here {"<"}
+                <div className="main_container">
+                    <h3>-- Content goes here --</h3>
+                </div>
+                <div className="pic_container">
+
+                    <div className="logos_container">
+                        <img src="../public/img/buy-comics-digital-comics.png" alt="" />
+                        <p>DIGITAL COMICS</p>
+                    </div>
+
+                    <div className="logos_container">
+                        <img src="../public/img/buy-comics-merchandise.png" alt="" />
+                        <p>DC MERCHANDISE</p>
+                    </div>
+
+                    <div className="logos_container">
+                        <img src="../public/img/buy-comics-subscriptions.png" alt="" />
+                        <p>DIGITAL COMICS</p>
+                    </div>
+
+                    <div className="logos_container">
+                        <img src="../public/img/buy-comics-shop-locator.png" alt="" />
+                        <p>DIGITAL COMICS</p>
+                    </div>
+
+                    <div className="logos_container">
+                        <img src="../public/img/buy-dc-power-visa.svg" alt="" />
+                        <p>DIGITAL COMICS</p>
+                    </div>
+
+
+
                 </div>
             </div>
         </main>
