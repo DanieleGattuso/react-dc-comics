3dd927c5f3db13c7215304267553df5d465cf7ca
daniele.gattuso <d.gattuso09@gmail.com>
2025-02-14 14:59:48 +0100

    centralizzato tutto il footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,30 +9,22 @@ export default function Footer(props) {
                     <div className="tiny_container">
                         <ul>
                             <li><span>DC COMICS</span></li>
-                            {appCategory1.map((category) => <li key={category.id}>{category.text}</li>)}
+                            {appCategory2.map((category) =>
+                                <li key={category.id}>{category.text}</li>)}
                         </ul>
 
                         <ul>
                             <li><span>SHOP</span></li>
-                            <li>Shop DC</li>
-                            <li>Shop DC Collectibles</li>
+                            {appCategory3.map((category) =>
+                                <li key={category.id}>{category.text} </li>)}
                         </ul>
                     </div>
 
                     <div className="tiny_container">
                         <ul>
                             <li><span>DC</span></li>
-                            <li> Terms Of Use</li>
-                            <li>Privacy policy (New)</li>
-                            <li>Ad Choices</li>
-                            <li>Advertising</li>
-                            <li>Jobs</li>
-                            <li>Subscriptions</li>
-                            <li>Talent Workshops</li>
-                            <li>CPSC Certificates</li>
-                            <li>Ratings</li>
-                            <li>Shop Help</li>
-                            <li>Contact Us</li>
+                            {appCategory1.map((category) =>
+                                <li key={category.id}>{category.text}</li>)}
                         </ul>
 
                     </div>
@@ -40,11 +32,8 @@ export default function Footer(props) {
                     <div className="tiny_container">
                         <ul>
                             <li><span>SITES</span></li>
-                            <li>DC</li>
-                            <li>MAD Magazine</li>
-                            <li>DC Kids</li>
-                            <li>DC Universe</li>
-                            <li>DC Power Visa</li>
+                            {appCategory4.map((category) =>
+                                <li key={category.id}>{category.text}</li>)}
                         </ul>
                     </div>
 
