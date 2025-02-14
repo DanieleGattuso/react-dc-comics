2167f1595e657affd55ed9b316cb43c1e1ecb45f
daniele.gattuso <d.gattuso09@gmail.com>
2025-02-14 10:14:30 +0100

    create array

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,42 @@ import Main from "./components/Main.jsx";
 
 function App() {
 
+  const categoryLinks1 = [
+    { text: 'Terms Of Use', url: '#', current: false },
+    { text: 'Privacy policy (New)', url: '#', current: false },
+    { text: 'Ad Choices', url: '#', current: false },
+    { text: 'Advertising', url: '#', current: false },
+    { text: 'Jobs', url: '#', current: false },
+    { text: 'Subscriptions', url: '#', current: false },
+    { text: 'Talent Workshop', url: '#', current: false },
+    { text: 'CPSC Certificates', url: '#', current: false },
+    { text: 'Ratings', url: '#', current: false },
+    { text: 'Shop Help', url: '#', current: false },
+    { text: 'Contact Us', url: '#', current: false },
+  ];
+
+  const categoryLinks2 = [
+    { text: 'Characters', url: '#', current: false },
+    { text: 'Comics', url: '#', current: false },
+    { text: 'Movies', url: '#', current: false },
+    { text: 'TV', url: '#', current: false },
+    { text: 'Games', url: '#', current: false },
+    { text: 'Videos', url: '#', current: false },
+    { text: 'News', url: '#', current: false },
+  ];
+
+  const categoryLinks3 = [
+    { text: 'Shop DC', url: '#', current: false },
+    { text: 'Shop DC Collectibles', url: '#', current: false },
+  ];
+
+  const categoryLinks4 = [
+    { text: 'DC', url: '#', current: false },
+    { text: 'MAD Magazine', url: '#', current: false },
+    { text: 'DC Magazine', url: '#', current: false },
+    { text: 'DC Universe', url: '#', current: false },
+    { text: 'DC Power Visa', url: '#', current: false },
+  ];
 
   return (
     <>
